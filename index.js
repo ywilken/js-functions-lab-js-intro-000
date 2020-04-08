cc4f310144c20849710f3c96041d9553491e92a2
Yannick Wilken <yannickwilken99@gmail.com>
2020-04-08 18:02:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ function happyHolidaysTo(name) {
 function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
